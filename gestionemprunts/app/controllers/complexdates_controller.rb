5ffef7b6743a6e6e065d4eb130a52cea0de577d8
Alexis Laurent <alexis.laurent2@gmail.com>
2014-01-11 10:47:55 +0100

    complexdate v2

--- gestionemprunts/app/controllers/complexdates_controller.rb
+++ gestionemprunts/app/controllers/complexdates_controller.rb
@@ -1,5 +1,4 @@
 class ComplexdatesController < ApplicationController
-  before_action :set_complexdate, only: [:show, :edit, :update, :destroy]
   
   def index
     @complexdates = Complexdate.all
@@ -7,36 +6,36 @@ class ComplexdatesController < ApplicationController
   
   def new
     @complexdate = Complexdate.new
-    puts "ici"
+
   end
   
   def create
     @complexdate = Complexdate.new(complexdate_params)
     if @complexdate.save
-      redirect_to complexdates_path, notice: 'Complexdate successfully added.'
+      redirect_to @complexdate, notice: 'Complexdate successfully added.'
     else
       render action: :new
     end
   end
   
   def edit
-    @complexdate = current_complexdate
+    @complexdate = Complexdate.find(params[:id])
+  end
+  
+  def show
+    @complexdate = Complexdate.find(params[:id])
   end
   
   def update
-    @complexdate = current_complexdate
-    if @complexdate.update(complexdate_params)
-      redirect_to complexdates_path, notice: 'Updated complexdate information successfully.'
+    @complexdate = Complexdate.find(params[:id])
+    if @complexdate.update_attributes(complexdate_params)
+      redirect_to @complexdate, notice: 'Updated complexdate information successfully.'
     else
       render action: 'edit'
     end
   end
   
   private
-    def set_complexdate
-      @complexdate = current_complexdate
-    end
-
     def complexdate_params
       params.require(:complexdate).permit(:cdate, :beginhour, :endhour, :precisehour)
     end
