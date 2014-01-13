00831d99996dc4a7d2fe647fcfb0407bfa42d294
suzy paeta <suzy.paeta@gmail.com>
2014-01-13 14:41:10 +0100

    Revert "scc"
    This reverts commit 00f1ffc0361c77dbb017a16301594d618aa21184.

--- gestionemprunts/db/migrate/20140113132128_remove_column_materialtype_from_technicalfeature.rb
+++
@@ -1,9 +0,0 @@
-class RemoveColumnMaterialtypeFromTechnicalfeature < ActiveRecord::Migration
-  def up
-    remove_column :technicalfeatures, :materialtype
-  end
-  
-  def down
-    add_column :technicalfeatures, :materialtype, :integer
-  end
-end
--- gestionemprunts/db/migrate/20140113132502_add_materialtype_id_to_technicalfeatures.rb
+++
@@ -1,5 +0,0 @@
-class AddMaterialtypeIdToTechnicalfeatures < ActiveRecord::Migration
-  def change
-    add_column :technicalfeatures, :materialtype, :integer
-  end
-end
