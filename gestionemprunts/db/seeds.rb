2772879eb8ebbddb0b200a068d37e9fc47013ead
suzy paeta <suzy.paeta@gmail.com>
2014-01-14 10:54:38 +0100

    seed

--- gestionemprunts/db/seeds.rb
+++ gestionemprunts/db/seeds.rb
@@ -18,6 +18,23 @@ type1.save
 type2 = Materialtype.create :label=>'Ordinateur'
 type2.save
 
-feature1 = Technicalfeature.create :os => 'Android', :name => 'Acer', :version => '2.4', :memory => '250', :ram => '8', :processorfrequency => '2.4', :materialtype=>type1
-mat1 = Material.create :serialnumber => 'JHFUER879GEDFE', :guaranteeend => Date.today, :anomaly => 'Aucune', :whobought => 'Nobody', :technicalfeature=>feature1
+feature1 = Technicalfeature.create :os => 'Android', :name => 'Acer', :version => '2.4', :memory => '250', :ram => '8', :processorfrequency => '2.4', :materialtype => type1
+mat1 = Material.create :serialnumber => 'JHFUER879GEDFE', :guaranteeend => Date.today, :anomaly => 'Aucune', :whobought => 'Nobody', :technicalfeature => feature1
 mat1.save
+
+compldatetype1 = Complexdatetype.create :label => 'RDV'
+compldatetype1.save
+
+compldatetype2 = Complexdatetype.create :label => 'Emprunt'
+compldatetype2.save
+
+reqstat1 = Requeststatus.create :label => 'Attente'
+reqstat1.save
+
+reqstat2 = Requeststatus.create :label => 'Terminee'
+reqstat2.save
+
+req1 = Request.create :borrowmotive => 'Cours', :user => romain, :requeststatus => reqstat1
+
+compldate1 = Complexdate.create :cdate => Date.new(2014, 01, 26), :beginhour => Time.now, :endhour => Time.now, :precisehour => Time.now, :request => req1, :complexdatetype => compldatetype1
+
